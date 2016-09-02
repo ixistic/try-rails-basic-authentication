3eda473866722efdcd3bc60360315671a3ea857d
ixistic <teeravipark@gmail.com>
2016-09-02 10:22:02 +0700

    Refactoring code

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -11,7 +11,7 @@ class SiteController < ApplicationController
 
   private
   def require_login
-    if !current_user
+    unless current_user.present?
       flash[:error] = "You must be logged in to access this section"
       redirect_to root_path
     end
